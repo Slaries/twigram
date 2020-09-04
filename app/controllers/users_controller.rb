6066ac332606cf1ffbf5500daae3e5295079e0bb
Slaries <slaries@rambler.ru>
2020-09-05 01:00:32 +0300

    block correct other profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = current_user.posts.order(created_at: :desc)
   end
+
   def edit
     @user = User.find(params[:id])
+    if current_user == @user
+      flash[:notice] = "Edit Profile"
+      # redirect_to edit_user_path(@user)
+    else
+      flash[:alert] = "Denided!"
+      redirect_to user_path(@user)
+    end
   end
+
   def update
     current_user.update(user_params)
     redirect_to edit_user_path(current_user)
