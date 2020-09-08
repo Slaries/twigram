a479dd51b456141775635112cf3caf93dab070f3
Slaries <slaries@rambler.ru>
2020-09-08 17:52:11 +0300

    correct follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :follower_follows, foreign_key: :following_id, class_name: "Follow"
   has_many :followers, through: :follower_follows, source: :follower
 
-  has_many :following_follows, foreign_key: :followel_id, class_name: "Follow"
+  has_many :following_follows, foreign_key: :follower_id, class_name: "Follow"
   has_many :followings, through: :following_follows, source: :following
 
   has_many :posts, dependent: :destroy
