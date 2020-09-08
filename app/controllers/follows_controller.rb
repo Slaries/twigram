c211924510a1ed70cfd38c426856071b95892e36
Slaries <slaries@rambler.ru>
2020-09-08 23:37:50 +0300

    add link to follow

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -8,7 +8,6 @@ class FollowsController < ApplicationController
     follower_user = current_user
     following_user = User.find(params[:following_id])
     Follow.create(follower: follower_user, following: following_user)
-    redirect_to user_following_index_path(current_user)
-    # redirect_to user_followers_path(current_user)
+    redirect_to user_followers_path(current_user)
   end
 end
