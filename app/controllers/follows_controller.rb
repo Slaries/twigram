ab70439e444d6285ef5cab3d862f7b059e554006
Slaries <slaries@rambler.ru>
2020-09-09 09:51:47 +0300

    correct show followers

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -7,7 +7,14 @@ class FollowsController < ApplicationController
   def create
     follower_user = current_user
     following_user = User.find(params[:following_id])
-    Follow.create(follower: follower_user, following: following_user)
-    redirect_to user_followers_path(current_user)
+    @follow = Follow.create(follower: follower_user, following: following_user)
+    if @follow.valid?
+      @follow.save
+    end
+    if @follow.save
+      redirect_to user_followers_path(following_user), flash: {success:"You follow now"}
+    else
+      redirect_to user_path(following_user), flash: {alert: "Some error"}
+    end
   end
 end
