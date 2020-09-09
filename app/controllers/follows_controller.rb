5cb93dfb893f1009044fdaa3191a9214562b36d5
Slaries <slaries@rambler.ru>
2020-09-09 18:38:09 +0300

    correct work unfollow button

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,9 +1,11 @@
 class FollowsController < ApplicationController
+
   def index
     @user = User.find(params[:id])
     @follower = @user.followers
     @followings = @user.followings
   end
+
   def create
     follower_user = current_user
     following_user = User.find(params[:following_id])
@@ -14,7 +16,20 @@ class FollowsController < ApplicationController
     if @follow.save
       redirect_to user_followers_path(following_user), flash: {success:"You follow now"}
     else
-      redirect_to user_path(following_user), flash: {alert: "Some error"}
+      redirect_to user_path(following_user), flash: {alert: "You already follow"}
+    end
+  end
+
+  def destroy
+    follow = Follow.find_by(follower_id: current_user.id, following_id: params[:user_id])
+    if follow.present?
+      follow.destroy
+    end
+   if follow.destroy
+      flash.now[:notice] = "Successfully deleted follow!"
+      redirect_to user_path(current_user)
+    else
+      flash[:alert] = "Error delete follow!"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'page/home'
   root to: 'page#home'
+  delete 'follows/:user_id' => 'follows#destroy'
   resources :users, only: [:show, :edit, :update] do resources :posts
   resources :followers, only: :index
   resources :following, only: :index
   end
-  resources :follows, only: [:create]
+  resources :follows, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
