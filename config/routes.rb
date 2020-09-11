ffc6ca95881b13d368c10f2d66c956a7e954b096
Slaries <slaries@rambler.ru>
2020-09-11 17:33:03 +0300

    show comments in post page and add comment

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   end
   resources :follows, only: [:create, :destroy]
   resources :feed_posts, only: :index
+  resources :comments, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
