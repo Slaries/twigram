1cdb16c5ed458adb246849740e28701ac6f9bfe5
Slaries <slaries@rambler.ru>
2020-09-19 20:19:17 +0300

    add OAuth with Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,31 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
   validates :name, presence: true, length: {maximum: 40}
   validates :email, presence: true, uniqueness:  { case_sensitive: false},
             format: { with: URI::MailTo::EMAIL_REGEXP }
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def apply_omniauth(auth)
+    update_attributes(
+        provider: auth.provider,
+        uid: auth.uid
+    )
+  end
+
+  def has_facebook_linked?
+    self.provider.present? && self.uid.present?
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  config.omniauth :facebook, Figaro.env.fb_app_id, Figaro.env.fb_app_secret, scope: 'name,email'
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
